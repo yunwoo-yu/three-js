60b2c04092343c86d81b02315650ff95af1eb1cc
Ycow <skypnal12@gmail.com>
2025-04-23 15:16:51 +0900

    feat: DragControls 각 오브젝트 드래그 이벤트 살펴보기

---
+++ 06/controls/src/ex06.js
@@ -0,0 +1,94 @@
+import * as THREE from "three";
+import { DragControls } from "three/examples/jsm/controls/DragControls";
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  3;
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  const meshs = [];
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let meterial;
+
+  for (let i = 0; i < 20; i++) {
+    meterial = new THREE.MeshStandardMaterial({
+      color: `rgb(
+				${Math.floor(50 + Math.random() * 205)},
+				${Math.floor(50 + Math.random() * 205)},
+				${Math.floor(50 + Math.random() * 205)}
+			)`,
+    });
+
+    mesh = new THREE.Mesh(geometry, meterial);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+    mesh.name = `box-${i}`;
+    scene.add(mesh);
+    meshs.push(mesh);
+  }
+
+  // Controls
+  const controls = new DragControls(meshs, camera, renderer.domElement);
+
+  controls.addEventListener("dragstart", (e) => {
+    console.log(e.object.name);
+  });
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    // FlyControls는 update안에 delta를 넣어줘야함
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
