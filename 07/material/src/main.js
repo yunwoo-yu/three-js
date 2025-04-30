aa898f43a6a1d1c476eb796c09ce483ecef786b9
Ycow <skypnal12@gmail.com>
2025-04-30 20:20:52 +0900

    feat: matcap material 적용해보기

--- 07/material/src/main.js
+++ 07/material/src/main.js
@@ -8,5 +8,6 @@
 // import example from "./ex08";
 // import example from "./ex09";
 // import example from "./ex10";
-import example from "./ex11";
+// import example from "./ex11";
+import example from "./ex12";
 example();
